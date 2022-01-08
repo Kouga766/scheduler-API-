e88724d2a3b8d52690d833470a079e2c3826ec4c
Kouga766 <houkenkokushi@gmail.com>
2022-01-08 08:14:41 +0000

    backend削除

--- backend/scheduler/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from .models import User, Task, TaskProcessed, Note, NoteProcessed
-
-
-@admin.register(User, Task, TaskProcessed, Note, NoteProcessed)
-class UserAdmin(admin.ModelAdmin):
-    pass
-
