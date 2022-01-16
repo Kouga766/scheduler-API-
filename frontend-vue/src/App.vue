f816711d8582696fd80664f31e82310a42a862ea
Kouga766 <houkenkokushi@gmail.com>
2022-01-16 13:15:42 +0000

    新規登録・ログイン画面

--- frontend-vue/src/App.vue
+++ frontend-vue/src/App.vue
@@ -1,27 +1,77 @@
 <template>
   <v-app>
-    <v-card>
-    <v-card-title>
-      <h1 class="display-1">ログイン</h1>
-    </v-card-title>
-    <v-card-text>
-      <v-form>
-        <v-text-field label="ユーザ名" />
-        <v-text-field label="パスワード" />
-      </v-form>
-    </v-card-text>
+  <v-card width="400px" class="mx-auto mt-5">
+   <v-tabs>
+     <v-tab>
+          <v-icon left>
+            mdi-login
+          </v-icon>
+          ログイン
+        </v-tab>
+        <v-tab>
+          <v-icon left>
+            mdi-account-plus
+          </v-icon>
+          新規登録
+        </v-tab>
+      <v-tab-item>
+
+          <v-img
+                :src="require('./assets/favicon.png')"
+                class="my-3"
+                contain
+                height="200"
+          />
+          <v-card-title>
+            <h1 class="display-1">ログイン</h1>
+          </v-card-title>
+          <v-card-text>
+            <v-form>
+              <v-text-field prepend-icon="mdi-account-circle" label="ユーザ名" />
+              <v-text-field prepend-icon="mdi-lock" append-icon="mdi-eye-off" label="パスワード" />
+              <v-card-actions class="justify-center">
+                <v-btn class="info">ログイン</v-btn>
+              </v-card-actions>
+            </v-form>
+          </v-card-text>
+
+      </v-tab-item>
+
+      <v-tab-item>
+
+          <v-img
+                :src="require('./assets/favicon.png')"
+                class="my-3"
+                contain
+                height="200"
+          />
+          <v-card-title>
+            <h1 class="display-1">新規登録</h1>
+          </v-card-title>
+          <v-card-text>
+            <v-form>
+              <v-text-field prepend-icon="mdi-account-circle" label="ユーザ名" />
+              <v-text-field prepend-icon="mdi-email" label="メールアドレス" />
+              <v-text-field prepend-icon="mdi-lock" append-icon="mdi-eye-off" label="パスワード" />
+              <v-text-field prepend-icon="mdi-lock" append-icon="mdi-eye-off" label="パスワード（再）" />
+              <v-card-actions class="justify-center">
+                <v-btn class="info">ログイン</v-btn>
+              </v-card-actions>
+            </v-form>
+          </v-card-text>
+
+      </v-tab-item>
+    </v-tabs>
   </v-card>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
-
 export default {
   name: 'App',
 
   components: {
-    HelloWorld,
+
   },
 
   data: () => ({
