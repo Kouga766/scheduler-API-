e97b1c686ee181796d97f70ea1995732467c2a3d
Kouga766 <houkenkokushi@gmail.com>
2022-03-06 15:08:16 +0000

    整理

--- frontend-vue/src/components/Main.vue
+++ frontend-vue/src/components/Main.vue
@@ -204,38 +204,38 @@
    </v-col>
 
    <v-col cols="3"  height="100%">
-   <v-card style="text-align:center" >
-     <div>
-       <h2>
-         お疲れ様です(@name)さん
-       </h2>
-       <h2>
-         あなたの本日のタスクは（@task）件です。
-       </h2>
-     </div>
+     <v-card style="text-align:center" >
+       <div>
+         <h2>
+           お疲れ様です(@name)さん
+         </h2>
+         <h2>
+           あなたの本日のタスクは（@task）件です。
+         </h2>
+       </div>
 
-     <v-card>
        <v-card>
-        <v-list
-        class="overflow-y-auto mx-auto my-10"
-        width="100%"
-        max-height="200"
-        max-width="500"
-        color="#98fb98">
-        <v-list-item
-            v-for="item in items"
-            :key="item.message">
-            {{ item.message }}
-        </v-list-item>
-    </v-list>
+         <v-card>
+          <v-list
+          class="overflow-y-auto mx-auto my-10"
+          width="100%"
+          max-height="200"
+          max-width="500"
+          color="#98fb98">
+          <v-list-item
+              v-for="item in items"
+              :key="item.message">
+              {{ item.message }}
+          </v-list-item>
+      </v-list>
+         </v-card>
        </v-card>
+      <div>
+        <h2>
+          『本日も頑張ってください！』
+        </h2>
+      </div>
      </v-card>
-    <div>
-      <h2>
-        『本日も頑張ってください！』
-      </h2>
-    </div>
-   </v-card>
    </v-col>
    </v-row>
    </v-container>
